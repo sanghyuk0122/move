5f22fa6d4885e08d72a4badad4c25f81fbc99df4
sanghyuk0122 <sanghyuk0122@gmail.com>
2026-06-30 20:02:26 +0900

    move the cube

---
+++ Script/Player.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float moveX;
+    float moveZ;
+
+    public float Speed;
+    Vector3 moveVec;
+
+    private void Update()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        moveZ = Input.GetAxis("Vertical");
+
+        moveVec = new Vector3(moveX, 0, moveZ).normalized;
+        transform.position += moveVec * Speed * Time.deltaTime;
+
+    }
+}
